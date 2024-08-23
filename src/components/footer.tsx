381c0d76b3a6dc322793f89fbaddc8eb185d5393
euotiniel <otonielemanuel21@gmail.com>
2024-08-23 11:16:21 +0100

    update: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,23 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import Link from "next/link";
-import { calculateTimeElapsed } from '@/lib/time';
+import { calculateTimeElapsed } from "@/lib/time";
 
 export default function Footer() {
-  const [timeElapsed, setTimeElapsed] = useState('');
-  const [timeUnit, setTimeUnit] = useState('');
+  const [timeElapsed, setTimeElapsed] = useState("");
+  const [timeUnit, setTimeUnit] = useState("");
 
   useEffect(() => {
-    const storedStartTime = sessionStorage.getItem('startTime');
+    const storedStartTime = sessionStorage.getItem("startTime");
     const currentTime = new Date().getTime();
 
-    const startTime = storedStartTime ? parseInt(storedStartTime, 10) : currentTime;
+    const startTime = storedStartTime
+      ? parseInt(storedStartTime, 10)
+      : currentTime;
     if (!storedStartTime) {
-      sessionStorage.setItem('startTime', currentTime.toString());
+      sessionStorage.setItem("startTime", currentTime.toString());
     }
 
     const updateTime = () => {
@@ -31,27 +33,41 @@ export default function Footer() {
   }, []);
 
   const getTimeUnit = (elapsedTime: any) => {
-    const [unit] = elapsedTime.split(' ');
+    const [unit] = elapsedTime.split(" ");
     return unit;
   };
 
   return (
     <footer className="w-full flex flex-col md:flex-row items-center justify-between text-neutral-600 dark:text-neutral-400">
       <div>
-        <small className="text-[13.5px] font-normal leading-none">
+        <p className="text-[13.5px] font-normal leading-none">
           2024 &copy;{" "}
-          <Link href="https://twitter.com/euotiniel">euotiniel</Link>
-        </small>
+          <Link
+            href="https://twitter.com/euotiniel"
+            className="font-medium underline underline-offset-4"
+          >
+            euotiniel
+          </Link>{" "}
+          . The source code is available on{" "}
+          <Link
+            href={"https://github.com/euotiniel/noticraft"}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            GitHub
+          </Link>
+        </p>
       </div>
       <div className="mt-4 flex items-center text-center">
-      <div className="mr-2 h-2 w-2 rounded-full bg-green-300 dark:bg-green-800 animate-pulse"></div>
+        <div className="mr-2 h-2 w-2 rounded-full bg-green-300 dark:bg-green-800 animate-pulse"></div>
         <div>
           <motion.p className="text-[13.5px]">
-          It's been on for{' '}
+            It's been on for{" "}
             <motion.span
               key={timeElapsed}
-              initial={{ opacity: 0, filter: 'blur(4px)' }}
-              animate={{ opacity: 1, filter: 'blur(0px)' }}
+              initial={{ opacity: 0, filter: "blur(4px)" }}
+              animate={{ opacity: 1, filter: "blur(0px)" }}
               transition={{ duration: 0.4 }}
             >
               {timeElapsed}
